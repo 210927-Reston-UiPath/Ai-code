4eb56911085e9a9c0ae3f9de1aaae854a332a74d
Ai Krasner <aikrasner@gmail.com>
2021-09-30 12:42:59 -0700

    added phonebook app

---
+++ PhonebookApp/Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim menu as Imenu = new MainMenu()
+        menu.Start()
+        Console.WriteLine("Hello World")
+    End Sub
+End Module
